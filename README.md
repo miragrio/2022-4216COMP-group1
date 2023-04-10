ac71b6c1eb0e300489b912bea1a513ab3800ece7
Patrick <1patrickbonk@gmail.com>
2023-04-10 22:42:06 +0100

    Second Graph + updated README  - Patrick

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 2022-4216COMP-group1
+# Information gathered from Los Angeles crimes data set (2020-2023).
+# Selection menu made for ease of use, description of graph is added to the menu selection.
+# 
--- template.py
+++ template.py
@@ -3,37 +3,8 @@ import pandas as pd
 
 file = pd.read_csv("CrimeDataSet.csv")
 
-def dan_1():
-    """Graph comparing gender ratios"""
-    Male = 0
-    Female = 0
-    Other = 0
-    search = {}
-    chosen_data = "Vict Sex"
-
-    for code in file[chosen_data]:
-        search[code] = search.get(code, 0)
-        search[code] += 1
-
-    Male = (search["M"])
-    Female = (search["F"])
-    Other = (search["X"])
-
-    print(f"Male: {Male}")
-    print(f"Female: {Female}")
-    print(f"Other: {Other}")
-    
-    names = ['Male', 'Female', 'Other']
-    values = [Male, Female, Other]
-
-    fig, ax = plt.subplots() 
-    ax.set_title("Gender Ratio")
-    ax.bar(names, values)
-    plt.show()
-
-
 def pat_1():
-    """ Bar Chart Representing Common Crimes """
+    """ Bar Chart Representing Crimes In Los Angeles (2020-2023) """
 
     # A dictionary consisting of crime code (Crm Cd) descriptions (Crm Cd Desc).
     codeDesc = {}
@@ -71,6 +42,94 @@ def pat_1():
     
     plt.show()
 
+def pat_2():
+    """ Depicitng Los Angeles Areas By Number Of Crimes (2020-2023) """
+    
+    # A dictionary to store area code and area name.
+    areaName = {}
+    for area_code, area_name in zip(file['AREA'], file['AREA NAME']):
+        areaName[area_code] = area_name
+
+    # Number of crime incidents, calculated from the 'AREA' column and incremented by 1.
+    get_amount = {}
+    for area_code in file['AREA']:
+        get_amount[area_code] = get_amount.get(area_code, 0)
+        get_amount[area_code] += 1
+        
+
+    # Looping through areaName dictionary, and printing the number of crime incidents
+    # with area code and area name included, with appropriate formatting.
+    for area_code, area_name in areaName.items():
+        amount = get_amount.get(area_code,0)
+        print(f"{area_code}: {area_name} -> ({amount} incidents of crime)")
+
+    # Taking the number of 'AREA NAME' instances, and gathering the top 5 and bottom 5 areas
+    # by number of instances. Columns created with value_counts function.
+    most_common = file['AREA NAME'].value_counts().nlargest(5).reset_index()
+    most_common.columns = ['AREA NAME', 'COUNT']
+
+    
+    least_common = file['AREA NAME'].value_counts().nsmallest(5).reset_index()
+    least_common.columns = ['AREA NAME', 'COUNT']
+
+    # Readable font selection for graphs.
+    font1 = {'family':'serif','color':'blue','size':30}
+    font2 = {'family':'serif','color':'black','size':20}
+
+    # Two figures to displaying safest and most dangerous regions.
+    fig, (left,right) = plt.subplots(1, 2)
+
+    # Plotting the most dangerous areas in the left bar chart.
+    left.bar(most_common['AREA NAME'], most_common['COUNT'],color='red')
+    left.grid(visible=True,linestyle='--')
+    left.set_facecolor('#E6E6FA')
+    spacing = 0.200
+    left.set_title("Dangerous Regions", fontdict=font1)
+    left.set_xlabel("Area Name",fontdict=font2)
+    left.set_ylabel("Amount of Crime (ALL CRIME INCLUDED!)", fontdict=font2)
+
+    # Plotting the safest areas in the right bar chart.
+    right.bar(least_common['AREA NAME'], least_common['COUNT'],color='green')
+    right.grid(visible=True,linestyle='--')
+    right.set_facecolor('#E6E6FA')
+    spacing = 0.200
+    right.set_title("Safest Regions",fontdict=font1)
+    right.set_xlabel("Area Name",fontdict=font2)
+    right.set_ylabel("Amount of Crime (All CRIME INCLUDED!)",fontdict=font2)
+
+    # Adjusting the width between suplots and displaying them.
+    plt.subplots_adjust(wspace=0.5)
+    plt.show()
+
+def dan_1():
+    """Graph comparing gender ratios"""
+    Male = 0
+    Female = 0
+    Other = 0
+    search = {}
+    chosen_data = "Vict Sex"
+
+    for code in file[chosen_data]:
+        search[code] = search.get(code, 0)
+        search[code] += 1
+
+    Male = (search["M"])
+    Female = (search["F"])
+    Other = (search["X"])
+
+    print(f"Male: {Male}")
+    print(f"Female: {Female}")
+    print(f"Other: {Other}")
+    
+    names = ['Male', 'Female', 'Other']
+    values = [Male, Female, Other]
+
+    fig, ax = plt.subplots() 
+    ax.set_title("Gender Ratio")
+    ax.bar(names, values)
+    plt.show()
+
+
 def mat_1():
     # Bar chart to represent locations with most activity
 
@@ -104,20 +163,22 @@ def mat_1():
 
 # Graph Selection.
 user_menu = {
-    "1": dan_1,
-    "2": pat_1,
-    "3": mat_1,
-    "4": quit
+    "1": pat_1,
+    "2": pat_2,
+    "3": dan_1,
+    "4": mat_1,
+    "5": quit
     }
 
 # Update when you upload your graph.
 while True:
     print('''
 Please select team member graph:
-1) Daniel
-2) Patrick
-3) Matei
-4) Quit
+1) Patrick - MOST COMMON CRIMES
+2) Patrick - CRIME IN LOS ANGELES AREAS
+3) Daniel - CRIME BY GENDER
+4) Matei - CRIME BY PREMISE
+5) Quit
 ''')
     user_choice = input("Selection: ")
 
